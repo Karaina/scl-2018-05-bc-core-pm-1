18d738ac76ef3a86c23d53fbd2fe9d6839575512
Karina Smoje Gueico <karinasmoje@gmail.com>
2018-05-31 17:31:42 -0300

    Añadí estilo al h3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function obtenerInformacion (){
 let offset = parseInt(document.getElementById("obtenernumero").value);
 let string = document.getElementById("escribirmensaje").value;
-document.getElementById("mensajeCifrado").innerText= window.cipher.encode(offset,string);
+document.getElementById("mensajeCifrado").innerText= window.cipher.encode(string, offset);
 }
 function obtenerInformacion2 (){
-    let offset = parseInt(document.getElementById("obtenernumero").value);
-    let string = document.getElementById("escribirmensaje").value;
-    document.getElementById("mensajedescifrado").innerText= window.cipher.decode(offset,string);
+let offset = parseInt(document.getElementById("obtenernumero").value);
+let string = document.getElementById("escribirmensaje").value;
+document.getElementById("mensajedescifrado").innerText= window.cipher.decode(string, offset);
 }
