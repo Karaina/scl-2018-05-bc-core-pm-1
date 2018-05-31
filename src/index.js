ebc1fc622c7ae34864737891810beee01d8bc062
Karina Smoje Gueico <karinasmoje@gmail.com>
2018-05-31 14:17:06 -0300

    Añadí función para guardar la información del usuario

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-/* Acá va tu código */
+function obtenerInformacion (){
+let offset = parseInt(document.getElementById("obtenernumero").value);
+let string = document.getElementById("escribirmensaje").value;
+document.getElementById("mensajeCifrado").innerText= window.cipher.encode(offset,string);
+}
+function obtenerInformacion2 (){
+    let offset = parseInt(document.getElementById("obtenernumero").value);
+    let string = document.getElementById("escribirmensaje").value;
+    window.cipher.decode(offset,string);
+}
